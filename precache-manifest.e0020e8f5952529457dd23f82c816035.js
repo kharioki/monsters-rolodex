bef07f66b37809276ab215b9501d4124a66dc9a8
kharioki <antolisah@gmail.com>
2019-12-15 13:40:37 +0300

    Updates

---
+++ precache-manifest.e0020e8f5952529457dd23f82c816035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a7856f90cbe2a757d9aa65b5635181",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2c3280975afbc53b07c",
+    "url": "/monsters-rolodex/static/css/main.4aca691c.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2c3280975afbc53b07c",
+    "url": "/monsters-rolodex/static/js/main.41d27ce8.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
